f55d226d5d4dba0f1f3a2c02bbae7e70cfff163b
pimpbot9000 <t.valimaki@gmail.com>
2022-01-22 19:09:21 +0200

    Easy eaquation generator

--- src/pages/random/equations-easy.js
+++ src/pages/random/equations-easy.js
@@ -32,33 +32,17 @@ const EquationsPage = () => {
             ["Jaanalla on _ omenaa enemmän kuin Leevillä.", ["kaksi", "kolme", "neljä"], [2, 3, 4], (n, k) => n - k]
         ]
 
-        const options2 = [
-            ["Elisalla on _ omenaa enemmän kuin Jaanalla.", ["kolme", "neljä", "viisi"], [3, 4, 5], (n, k) => n + k],
-            ["Jaanalla on _ omenaa enemmän kuin Elisalla.", ["kolme", "neljä", "viisi"], [3, 4, 5], (n, k) => n - k],
-            ["Elisan omenoiden lukumäärä on _ Jaanan omenoiden lukumäärään nähden.", ["kaksinkertainen", "kolminkertainen", "nelinkertainen"], [2, 3, 4], (n, k) => n * k], 
-        ]
-
         const option = choose(options)
         const idx = getRandomIndex(option[1])
         const str = option[0].replace('_', option[1][idx])
         const k = option[2][idx]
         const f = option[3]
         const leevi = f(jaana, k)
-        
-
-        const option2 = choose(options2)
-        const idx2 = getRandomIndex(option2[1])
-        const str2 = option2[0].replace('_', option2[1][idx2])
-        const k2 = option2[2][idx2]
-        const f2 = option2[3]
-        const elisa = f2(jaana, k2)
-
-        const sumStr = sumString.replace('_', jaana + leevi + elisa)
-        const resStr = choose([<> {str} {str2} </>, <> {str2} {str} </> ])
+        const sumStr = sumString.replace('_', jaana + leevi)
         return (
             <div>
-                <div>{sumStr} {resStr} Laske Leevin, Jaanan ja Elisan omenoiden lukumäärät.</div>
-                <div>(ratkaisu: Jaana: {jaana}, Leevi: {leevi}, Elisa: {elisa})</div>
+                <div>{sumStr} {str} Laske Leevin ja Jaanan omenoiden lukumäärät.</div>
+                <div>(ratkaisu: Jaana: {jaana}, Leevi: {leevi})</div>
                 <br/>
             </div>
         )
