ac1b214321bc5d721ed3b3e0873c68f447bd4ceb
Tuomas Välimäki <t.valimaki@gmail.com>
2024-02-24 21:16:03 +0200

    Simplify turtle page

--- src/pages/turtle.js
+++ src/pages/turtle.js
@@ -4,9 +4,9 @@ import Segment from '../components/segment'
 import Obs from '../components/obs'
 import Code from '../components/code'
 const TurtlePage = () => {
-    return <Layout pageTitle="Piirustamista kilpparilla">
-        <iframe src="https://trinket.io/embed/python/413c450ce5?runOption=run" width="100%" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
-    </Layout>
+    
+        return(<iframe src="https://trinket.io/embed/python/413c450ce5?runOption=run" width="100%" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>)
+    
 }
 
 export default TurtlePage
